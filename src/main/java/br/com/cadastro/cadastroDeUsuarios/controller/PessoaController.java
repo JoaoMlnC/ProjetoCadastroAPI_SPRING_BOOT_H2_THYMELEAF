3d6c14294905b6f1808d81b24f1d6105c1221945
JoaoMlnC <joaomlncampos@hotmail.com>
2022-09-11 13:03:55 -0300

    Finalizando projeto

--- src/main/java/br/com/cadastro/cadastroDeUsuarios/controller/PessoaController.java
+++ src/main/java/br/com/cadastro/cadastroDeUsuarios/controller/PessoaController.java
@@ -1,9 +1,12 @@
 package br.com.cadastro.cadastroDeUsuarios.controller;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import br.com.cadastro.cadastroDeUsuarios.hr.repoditory.PessoaRepository;
@@ -30,9 +33,33 @@ public class PessoaController {
 		return "rh/pessoas/form";
 	}
 	
+	@GetMapping("/rh/pessoas/{id}")
+	public String alterarPessoa(@PathVariable("id")long id, Model model) {
+		Optional<Pessoa> pessoaOpt = pessoaRepository.findById(id);
+		if (pessoaOpt.isEmpty()) {
+			throw new IllegalArgumentException("Pessoa Inválida");
+		}
+		
+		model.addAttribute("pessoa", pessoaOpt.get());
+		return "rh/pessoas/form";
+		
+	}
+	
 	@PostMapping("rh/pessoas/salvar")
 	public String salvarPessoa(@ModelAttribute("pessoa") Pessoa pessoa) {
 		pessoaRepository.save(pessoa);
 		return"redirect:/rh/pessoas";
+	}
+	
+	@GetMapping("/rh/pessoas/excluir/{id}")
+	public String excluirPessoa(@PathVariable("id") long id) {
+		Optional<Pessoa> pessoaOpt = pessoaRepository.findById(id);
+		if (pessoaOpt.isEmpty()) {
+			throw new IllegalArgumentException("Pessoa Inválida");
+		}
+		
+		pessoaRepository.delete(pessoaOpt.get());
+		return "redirect:/rh/pessoas";
+
 	}
 }
